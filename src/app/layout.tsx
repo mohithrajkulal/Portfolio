ae891fc5502a9a5eae80e9d56623ab76b39b17e8
Mohith Raj Kulal <mohith.kulal@codecraft.co.in>
2025-05-29 15:15:49 +0530

    build issue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import Navbar from "@/Components/Navbar";
 import Footer from "@/Components/Footer";
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
